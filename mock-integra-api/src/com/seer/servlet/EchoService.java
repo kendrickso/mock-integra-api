1a3d5e7bd7fde23a7213973b2178ddc8d2bdc58a
Kendrick So <kendrick.so@seer-technologies.com>
2016-01-22 01:26:18 +0800

    Initial project

---
+++ mock-integra-api/src/com/seer/servlet/EchoService.java
@@ -0,0 +1,30 @@
+package com.seer.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EchoService extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("application/json; charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		System.out.println("RequestURL="+req.getRequestURL());
+		System.out.println("QueryString="+req.getQueryString());
+		out.println("{\"data\":{\"result\":\"ok\",\"request_url\":\""+req.getRequestURL()+"\",\"query_string\":\""+req.getQueryString()+"\"}}");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+}
